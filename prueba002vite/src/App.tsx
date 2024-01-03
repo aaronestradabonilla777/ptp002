aadf343325a27c2d520333c435c88505200eb85f
aaronestradabonilla777 <lacomarcaz@hotmail.com>
2024-01-03 16:51:25 -0600

    create_table_and_data

--- prueba002vite/src/App.tsx
+++ prueba002vite/src/App.tsx
@@ -1,13 +1,31 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-   <main>
+    <main>
+      <aside>
+        <h1>Prueba Tecnica</h1>
+        <h2>Anadir y eliminar elemento de una lista</h2>
+        <form>
+          <label>
+            Elemento a introducir:
+            <input name="item" required type="text" placeholder="Videojuegos" />
+          </label>
+          <button> Anadir Elemento a la lista</button>
+        </form>
+      </aside>
 
-   </main>
-  )
+      <section>
+        <h2>Lista de Elementos</h2>
+        <ul>
+          <li>Videojuegos🎮</li>
+          <li>libros 📕</li>
+          <li>Series📺</li>
+          <li>Peliculas🎥</li>
+        </ul>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
